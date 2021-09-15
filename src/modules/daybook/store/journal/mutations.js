f0d578c189f5396d9a612ff5d1c7d7c367b5cd5a
FaytMx <silverzero55@gmail.com>
2021-09-14 21:03:36 -0500

    primer commit

---
+++ src/modules/daybook/store/journal/mutations.js
@@ -0,0 +1,14 @@
+// export const myMutation = async (state) => {
+
+// }
+
+export const setEntries = async (/*state*/) => {
+
+}
+
+export const updateEntries = async (/*state*/) => {
+
+}
+export const addEntries = async (/*state*/) => {
+
+}
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,36 @@
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi, velit fugit omnis iste corrupti accusamus reprehenderit dolore facilis ipsum tempore, quod voluptas et, eaque nisi in hic minima at vero.`,
+      picture: null,
+    },
+
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi, velit fugit omnis iste corrupti accusamus reprehenderit dolore facilis ipsum tempore, quod voluptas et, eaque nisi in hic minima at vero.`,
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toDateString(),
+      text: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi, velit fugit omnis iste corrupti accusamus reprehenderit dolore facilis ipsum tempore, quod voluptas et, eaque nisi in hic minima at vero.`,
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 3000,
+      date: new Date().toDateString(),
+      text: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi, velit fugit omnis iste corrupti accusamus reprehenderit dolore facilis ipsum tempore, quod voluptas et, eaque nisi in hic minima at vero.`,
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 4000,
+      date: new Date().toDateString(),
+      text: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi, velit fugit omnis iste corrupti accusamus reprehenderit dolore facilis ipsum tempore, quod voluptas et, eaque nisi in hic minima at vero.`,
+      picture: null,
+    },
+  ],
+});
